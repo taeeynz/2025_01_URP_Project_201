38e44d2f67723742908078583b7892b5047aa36f
taeeynz <jgim7122@gmail.com>
2025-05-13 11:10:37 +0900

    [Feat] 잔상 효과 구현

---
+++ Assets/Script/MeshTrail.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MeshTrail : MonoBehaviour
+{
+    public float activeTime = 2.0f;
+    public MovementInput moveScript;
+    public float speedBoost = 6;
+    public Animator animator;
+    public float animSpeedBoost = 1.5f;
+
+    [Header("Mesh Releted")]
+    public float meshRefreshRate = 1.0f;
+    public float meshDestroyDelay = 3.0f;
+    public Transform positionToSpawn;
+
+    [Header("Shader Releted")]
+    public Material mat;
+    public string shaderVerRef;
+    public float shaderVarRate = 0.1f;
+    public float shaderVarRefreshRate = 0.05f;
+
+    private SkinnedMeshRenderer[] skinnedRenderer;
+    private bool isTrailActive;
+
+    private float bormalSpeed;
+    private float normalAnimSpeed;
+
+    IEnumerator AnimateMaterialFloat(Material m, float valueGoal, float rate, float refreshRate)
+    {
+        float valueToAnimate = m.GetFloat(shaderVerRef);
+
+
+        while (valueToAnimate > valueGoal)
+        {
+            valueToAnimate -= rate;
+            m.SetFloat(shaderVerRef, valueToAnimate);
+            yield return new WaitForSeconds(refreshRate);
+        }
+    }
+
+    IEnumerator ActivateTrail(float timeActivated)
+    {
+        normalAnimSpeed = moveScript.movementSpeed;
+        moveScript.movementSpeed = speedBoost;
+
+        normalAnimSpeed = animator.GetFloat("animSpeed");
+        animator.SetFloat("animSpeed", animSpeedBoost);
+
+        while (timeActivated > 0)
+        {
+            timeActivated -= meshRefreshRate;
+
+            if (skinnedRenderer == null)
+                skinnedRenderer = positionToSpawn.GetComponentsInChildren<SkinnedMeshRenderer>();
+
+            for(int i = 0; i < skinnedRenderer.Length; i++)
+            {
+                GameObject gObj = new GameObject();
+                gObj.transform.SetPositionAndRotation(positionToSpawn.position, positionToSpawn.rotation);
+
+                MeshRenderer mr = gObj.GetComponent<MeshRenderer>();
+                MeshFilter mf= gObj.AddComponent <MeshFilter>();
+
+                Mesh m = new Mesh();
+                skinnedRenderer[i].BakeMesh(m);
+                mf.mesh = m;
+                mr.material = mat;
+
+                StartCoroutine(AnimateMaterialFloat(mr.material, 0, shaderVarRate, shaderVarRefreshRate));
+
+                Destroy(gObj, meshDestroyDelay);
+            }
+
+            yield return new WaitForSeconds(meshRefreshRate);
+        }
+
+
+        moveScript.movementSpeed = normalAnimSpeed;
+        animator.SetFloat("animSpeed", normalAnimSpeed);
+        isTrailActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isTrailActive)
+        {
+            isTrailActive = true;
+            StartCoroutine(ActivateTrail(activeTime));
+        }
+    }
+}
